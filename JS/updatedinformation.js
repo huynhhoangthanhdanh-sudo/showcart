f185af3c399d913163647fa21cbec49efd0f4ed1
Danh3426 <huynhhoangthanhdanh@gmail.com>
2026-05-23 03:11:41 +0000

    trust again

--- JS/updatedinformation.js
+++ JS/updatedinformation.js
@@ -120,6 +120,7 @@ function closeupdatedbutton() {
   sexclosebutton.addEventListener("click", () => close(updatedsex));
 }
 document.addEventListener("DOMContentLoaded", closeupdatedbutton);
+
 function finduserinuserlist(currentUser, userlist) {
   for (let i = 0; i < userlist.length; i++) {
     if (
@@ -308,18 +309,38 @@ function updatedinformation() {
 document.addEventListener("DOMContentLoaded", updatedinformation);
 
 function changeavatar(event) {
+  const file = event.target.files[0];
+  if(!file) return;
+  const useravatar = new FileReader();
+
   const userlist = JSON.parse(window.localStorage.getItem("userlist")) || [];
   const currentUser = JSON.parse(window.localStorage.getItem("currentUser"));
   const userData = finduserinuserlist(currentUser, userlist);
 
-  const useravatar = new FileReader();
-  useravatar.onload = function () {
-    const image = document.getElementById("avatar");
-    userData.avatar = useravatar.result;
-    currentUser.avatar = useravatar.result;
-    window.localStorage.setItem("userlist", JSON.stringify(userlist));
-    window.localStorage.setItem("currentUser", JSON.stringify(currentUser));
-    image.src = userData.avatar;
-  };
-  useravatar.readAsDataURL(event.target.files[0]);
+  useravatar.onload = function(e){
+    const img = new Image();
+    img.src = e.target.result;
+
+    img.onload = function(){
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+      const size = 150;
+      canvas.width = size;
+      canvas.height = size;
+      ctx.drawImage(img,0,0,size,size);
+      const imgURL = canvas.toDataURL("image/jpeg",0.7);
+
+      if(currentUser){
+        currentUser.avatar = imgURL;
+        userData.avatar = imgURL;
+      }
+      window.localStorage.setItem("userlist",JSON.stringify(userlist));
+      window.localStorage.setItem("currentUser",JSON.stringify(currentUser));
+
+      const imageavatar = document.getElementById("avatar");
+      imageavatar.src = imgURL;
+    }
+  }
+  useravatar.readAsDataURL(file);
 }
+
