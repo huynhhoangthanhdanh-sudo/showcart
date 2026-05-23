2fb50a75f65c073e0a18d0e3f3fb5dfa37237ace
Danh3426 <huynhhoangthanhdanh@gmail.com>
2026-05-23 03:05:54 +0000

    trust bro

--- JS/changeavatar.js
+++ JS/changeavatar.js
@@ -12,17 +12,37 @@ function finduser(signinusername, signinpassword, userlist) {
   }
 }
 function changeavatar(event) {
+  const file = event.target.files[0];
+  if(!file) return;
+  const useravatar = new FileReader();
+
   const userlist = JSON.parse(window.localStorage.getItem("userlist")) || [];
   const currentUser = JSON.parse(window.localStorage.getItem("currentUser"));
   const userData = finduser(currentUser, userlist);
-  const useravatar = new FileReader();
-  useravatar.onload = function () {
-    userData.avatar = useravatar.result;
-    currentUser.avatar = useravatar.result;
-    window.localStorage.setItem("userlist", JSON.stringify(userlist));
-    window.localStorage.setItem("currentUser", JSON.stringify(currentUser));
-    const image = document.getElementById("avatar");
-    image.src = useravatar.result;
-  };
-  useravatar.readAsDataURL(event.target.files[0]);
+
+  useravatar.onload = function(e){
+    const img = new Image();
+    img.src = e.target.result;
+
+    img.onload = function(){
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+      const size = 150;
+      canvas.width = size;
+      canvas.height = size;
+      ctx.drawImage(img,0,0,size,size);
+      const imgURL = canvas.toDataURL("image/jpeg",0.7);
+
+      if(currentUser){
+        currentUser.avatar = imgURL;
+        userData.avatar = imgURL;
+      }
+      window.localStorage.setItem("userlist",JSON.stringify(userlist));
+      window.localStorage.setItem("currentUser",JSON.stringify(currentUser));
+
+      const imageavatar = document.getElementById("avatar");
+      imageavatar.src = imgURL;
+    }
+  }
+  useravatar.readAsDataURL(file);
 }
